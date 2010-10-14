fc0dff21b71c0b1d6a88f3766ff0b3f2ef923540
docunext <albert.lash@docunext.com>
2010-10-13 22:13:13 -0400

    errors

--- example/pw.rb
+++ example/pw.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby1.9.1
 require 'password'
 
-# This example should work with both 1.8 and 1.9
+# This example should work with 1.8 and 1.9
 ctpw = ARGV[0]
 
 #ctpw = `xxd -l 3 -p /dev/random`
 
-puts ctpw
-
-cleartext = ctpw
-puts cleartext
-password = Password.new(cleartext)
-crypted = password.crypt(Password::MD5)
-
-puts crypted
+unless ctpw.nil?
+  puts ctpw
+  password = Password.new(ctpw)
+  crypted = password.crypt(Password::MD5)
+  puts crypted
+else
+  puts "No password was provided."
+end
